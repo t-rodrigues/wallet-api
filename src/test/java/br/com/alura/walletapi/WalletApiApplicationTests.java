c7c605ca44d75b4badfd2a3841ee7f19c22f0645
Thiago Rodrigues <thiagor_@live.com>
2021-09-10 21:12:26 -0300

    initial commit

---
+++ src/test/java/br/com/alura/walletapi/WalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.walletapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
