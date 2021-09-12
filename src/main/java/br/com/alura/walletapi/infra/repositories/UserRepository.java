86736799a7b97fa2592f0dff62fe26bb2d30d6f4
Thiago Rodrigues <thiagor_@live.com>
2021-09-12 15:45:04 -0300

    feat: add UserRepository

---
+++ src/main/java/br/com/alura/walletapi/infra/repositories/UserRepository.java
@@ -0,0 +1,25 @@
+package br.com.alura.walletapi.infra.repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.walletapi.domain.User;
+
+@Repository
+public class UserRepository {
+
+    private List<User> users = new ArrayList<>();
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        users.add(user);
+
+        return user;
+    }
+
+}
--- src/main/java/br/com/alura/walletapi/services/UserService.java
+++ src/main/java/br/com/alura/walletapi/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.alura.walletapi.services;
 
-import java.util.*;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
@@ -9,24 +10,29 @@ import org.springframework.stereotype.Service;
 import br.com.alura.walletapi.application.dtos.UserFormDto;
 import br.com.alura.walletapi.application.dtos.UserResponseDto;
 import br.com.alura.walletapi.domain.User;
+import br.com.alura.walletapi.infra.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Service
 public class UserService {
 
-    private List<User> users = new ArrayList<>();
     private ModelMapper modelMapper = new ModelMapper();
 
+    private final UserRepository userRepository;
+
     public List<UserResponseDto> getUsers() {
+        var users = userRepository.findAll();
+
         return users.stream().map(user -> modelMapper.map(user, UserResponseDto.class)).collect(Collectors.toList());
     }
 
-    public void createUser(UserFormDto user) {
-        User newUser = modelMapper.map(user, User.class);
+    public void createUser(UserFormDto userFormDto) {
+        User newUser = modelMapper.map(userFormDto, User.class);
         String password = new Random().nextInt(99999) + "";
 
         newUser.setPassword(password);
-
-        users.add(newUser);
+        userRepository.save(newUser);
     }
 
 }
