c40efcdcd6ad311450298524c6fc20ae34c2281a
Thiago Rodrigues <thiagor_@live.com>
2021-09-20 13:41:28 -0300

    feat: add User JpaRepository

--- src/main/java/br/com/alura/walletapi/infra/repositories/UserRepository.java
+++ src/main/java/br/com/alura/walletapi/infra/repositories/UserRepository.java
@@ -1,25 +1,11 @@
 package br.com.alura.walletapi.infra.repositories;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.walletapi.domain.entities.User;
 
 @Repository
-public class UserRepository {
-
-    private List<User> users = new ArrayList<>();
-
-    public List<User> findAll() {
-        return users;
-    }
-
-    public User save(User user) {
-        users.add(user);
-
-        return user;
-    }
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
