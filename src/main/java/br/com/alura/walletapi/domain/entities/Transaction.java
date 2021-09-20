5fef10b49fa722ed4fb76165f0dae1dda82e8acf
Thiago Rodrigues <thiagor_@live.com>
2021-09-20 13:08:09 -0300

    feat: add User and Transaction mapping

--- src/main/java/br/com/alura/walletapi/domain/entities/Transaction.java
+++ src/main/java/br/com/alura/walletapi/domain/entities/Transaction.java
@@ -3,18 +3,30 @@ package br.com.alura.walletapi.domain.entities;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.*;
+
 import br.com.alura.walletapi.domain.entities.enums.TransactionType;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "transactions")
 public class Transaction {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String ticker;
     private LocalDate date;
     private BigDecimal price;
     private Integer quantity;
+
+    @Enumerated(EnumType.STRING)
     private TransactionType type;
+
+    @ManyToOne
+    private User user;
 
 }
--- src/main/java/br/com/alura/walletapi/domain/entities/User.java
+++ src/main/java/br/com/alura/walletapi/domain/entities/User.java
@@ -1,12 +1,19 @@
 package br.com.alura.walletapi.domain.entities;
 
+import javax.persistence.*;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String email;
     private String password;
