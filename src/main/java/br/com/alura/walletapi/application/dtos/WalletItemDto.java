067b380f93b0b365d7a602b6d8f3bb060ca56710
Thiago Rodrigues <thiagor_@live.com>
2021-10-10 20:21:27 -0300

    fix: bugfix and simplify report query

--- src/main/java/br/com/alura/walletapi/application/dtos/WalletItemDto.java
+++ src/main/java/br/com/alura/walletapi/application/dtos/WalletItemDto.java
@@ -12,10 +12,11 @@ public class WalletItemDto {
     private Long quantity;
     private BigDecimal percent;
 
-    public WalletItemDto(String ticker, Long quantity, Double percent) {
+    public WalletItemDto(String ticker, Long quantity, Long totalQuantity) {
         this.ticker = ticker;
         this.quantity = quantity;
-        this.percent = BigDecimal.valueOf(percent).multiply(BigDecimal.valueOf(100)).setScale(2, RoundingMode.HALF_UP);
+        this.percent = BigDecimal.valueOf(quantity).divide(new BigDecimal(totalQuantity), 4, RoundingMode.HALF_UP)
+                .multiply(BigDecimal.valueOf(100)).setScale(2);
     }
 
 }
--- src/main/java/br/com/alura/walletapi/infra/repositories/TransactionRepository.java
+++ src/main/java/br/com/alura/walletapi/infra/repositories/TransactionRepository.java
@@ -12,8 +12,9 @@ import java.util.List;
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     @Query("SELECT NEW br.com.alura.walletapi.application.dtos.WalletItemDto("
-            + "t.ticker, SUM(t.quantity), SUM(t.quantity) * 1.0 / (SELECT SUM(t2.quantity) * 1.0 FROM Transaction t2)) "
-            + "FROM Transaction t GROUP BY t.ticker")
+            + "t.ticker, SUM(CASE WHEN(t.type = 'BUY') THEN t.quantity ELSE(t.quantity * -1) END), "
+            + "(SELECT SUM(CASE WHEN(t2.type = 'BUY') THEN t2.quantity ELSE(t2.quantity * -1) END) FROM Transaction t2)"
+            + ") FROM Transaction t GROUP BY t.ticker")
     List<WalletItemDto> reportInvestmentPortfolio();
 
 }
